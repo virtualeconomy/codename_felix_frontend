99a8b916d8b58d850b648c2ae9c9a57ba331c21b
Haynar216 <913636021@qq.com>
2022-01-31 15:35:22 +0800

    Update About.vue

--- src/views/Settings/About.vue
+++ src/views/Settings/About.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
     <i
-      class="el-icon-arrow-left"
-      @click="$router.go(-1)"
-      style="color:#FB8809;font-size:20px;font-weight:600"
+        class="el-icon-arrow-left"
+        @click="$router.go(-1)"
+        style="color:#FB8809;font-size:20px;font-weight:600;cursor:pointer"
     >back to settings</i>
     <h3>About us</h3>
+    <div style="margin-bottom:19px;font-family:'coves-light'">
+      Felix is the first dictionary on a blockchain.
+      It was inspired by The Immutable (DARA),
+      a community led project focused on immortalizingthe most valuable content and informationin the world today.Felix is powered by
+      V Systems technology.
+    </div>
     <div style="font-size:1.17em;font-weight:bold">Official website</div>
     <span style="color:#FB8809">saveaword.com</span>
     <div style="font-size:1.17em;font-weight:bold;margin-top:19px">Version</div>
@@ -18,7 +24,7 @@ export default {
   name: "About",
   data() {
     return {
-    }
+    };
   },
   methods: {},
   mounted() {
