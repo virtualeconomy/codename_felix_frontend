bf5d2871452d4743062496b408f5740d1e5bc270
Peerchemist <dx.1178@gmail.com>
2022-02-01 17:34:28 +0100

    prep for deployment on server

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   // publicPath: '/',
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/felix_frontend/'
+    ? '/'
     : '/',
   outputDir: 'dist',
   assetsDir: 'static',
