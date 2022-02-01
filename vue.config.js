740b9c466e9f5dd454f5cfe527dc6c4ef76082ee
Peerchemist <dx.1178@gmail.com>
2022-02-01 17:25:01 +0100

    new backend addr

--- vue.config.js
+++ vue.config.js
@@ -15,7 +15,7 @@ module.exports = {
       //   target: 'http://samblana.vos.systems:8000'
       // },
       '/': {
-        target: 'http://saveaword.com:8000'
+        target: 'https://saveaword.com/api'
       }
     }
   }
