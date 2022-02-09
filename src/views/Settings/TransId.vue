c00b144e439de454ba9b5f5322c8cfe714aaf39a
jwzheng96 <jianweizheng@pku.edu.cn>
2022-02-09 15:16:05 +0800

    add the init page of List of transactions ID

---
+++ src/views/Settings/TransId.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <i
+        class="el-icon-arrow-left"
+        @click="$router.go(-1)"
+        style="color:#FB8809;font-size:20px;font-weight:600;cursor:pointer"
+    >back to settings</i>
+    <h3>List of transactions ID</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TransId",
+  data() {
+    return {
+    };
+  },
+  methods: {},
+  mounted() {
+  }
+};
+</script>
+
+<style scoped>
+</style>
