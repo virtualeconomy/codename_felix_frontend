666b874ae7301965bb2452289d2bfe2c1608cf29
jwzheng96 <jianweizheng@pku.edu.cn>
2022-02-10 19:18:42 +0800

    implement the UI of txID list

--- src/views/Settings/TransId.vue
+++ src/views/Settings/TransId.vue
@@ -6,6 +6,14 @@
         style="color:#FB8809;font-size:20px;font-weight:600;cursor:pointer"
     >back to settings</i>
     <h3>List of transactions ID</h3>
+    <div class="transaction_detail" 
+        v-for="(item,index) in transaction_list"
+        :key="index">
+        <div class="session_status">#{{index+1}} SESSION - {{ item.word }}</div>
+        <div class="detail_content">{{item.txId}}</div>
+        <div class="session_status">STATUS</div>
+        <div class="detail_content">{{item.status}}</div>
+    </div>
   </div>
 </template>
 
@@ -14,6 +22,9 @@ export default {
   name: "TransId",
   data() {
     return {
+        transaction_list: [
+            {"word":"BOOK", "txId":"0x966e5D...3BA2D41701A", "status":"done"},{"word":"MATURE", "txId":"0x966e5D...3BA2D41701A", "status":"done"} 
+        ]
     };
   },
   methods: {},
@@ -23,4 +34,21 @@ export default {
 </script>
 
 <style scoped>
+.transaction_detail{
+    margin-top:14px;
+    height:120px;
+    border-bottom: solid black 1px;
+}
+.session_status {
+    color:171616;
+    font-size:20px;
+    font-weight:bold 
+}
+.detail_content {
+    color:#FB8809;
+    font-size:15px;
+    font-weight:bold;
+    margin-top: 7px;
+    margin-bottom: 7px;
+}
 </style>
