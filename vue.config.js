6bba27ef773825b7bc9af705a9c89e74e90163e3
jwzheng96 <jianweizheng@pku.edu.cn>
2022-02-10 22:40:10 +0800

    use the new backend url

--- vue.config.js
+++ vue.config.js
@@ -15,7 +15,8 @@ module.exports = {
       //   target: 'http://samblana.vos.systems:8000'
       // },
       '/': {
-        target: 'https://saveaword.com/api'
+        // target: 'https://saveaword.com/api'
+        target: 'http://163.172.101.111:8000'
       }
     }
   }
