2280798d25fe5dbb668cb56d8ad789c518da544e
Xavier-Gao <1184072847@qq.com>
2022-03-07 15:53:25 +0800

    add option dialog for donation

--- src/components/Bottom.vue
+++ src/components/Bottom.vue
@@ -1,13 +1,18 @@
 <template>
   <div id="bottom">
-    <img class="placeholderDiv" style="padding:10px;width:30px;" src="@/assets/imgs/footer_icon.svg" width="30" />
+    <img
+      class="placeholderDiv"
+      style="padding:10px;width:30px;"
+      src="@/assets/imgs/footer_icon.svg"
+      width="30"
+    />
     <div class="content" style="flex:1;color:white;text-align:center;">
-      <span class="placeholderDiv" style="font-weight:bold;color:#FB8809">MINTED WORDS: </span>
-      <span class="placeholderDiv" >{{ NFTCount }} </span>
-      <span class="placeholderDiv" style="font-weight:bold;color:#FB8809">&nbsp;|&nbsp;LAST MINTED: </span>
-      <span class="placeholderDiv" >{{ lastSavedWords }} </span>
+      <span class="placeholderDiv" style="font-weight:bold;color:#FB8809">MINTED WORDS:</span>
+      <span class="placeholderDiv">{{ NFTCount }}</span>
+      <span class="placeholderDiv" style="font-weight:bold;color:#FB8809">&nbsp;|&nbsp;LAST MINTED:</span>
+      <span class="placeholderDiv">{{ lastSavedWords }}</span>
       <span class="placeholderDiv" style="font-weight:bold;color:#FB8809">&nbsp;|&nbsp;</span>
-      <span class="balance_label" style="font-weight:bold;color:#FB8809"> Balance</span>
+      <span class="balance_label" style="font-weight:bold;color:#FB8809">Balance</span>
       &nbsp;
       <span class="balance_amount">{{ balance }} VSYS</span>
       <div @click="dialogFormVisible=true" class="dontate-btn">DONATE</div>
@@ -21,45 +26,75 @@
         >Immutable DAO</span>
       </div>
     </div>
-    <el-dialog
-      title="Thank you for your donation!"
-      :visible.sync="dialogFormVisible"
-      append-to-body
-    >
-      <el-form>
-        <div
-          style="font-size:20px;margin-bottom:10px"
-        >The money you donate will go into this account:</div>
-        <div style="color:#FB8809">{{ address }}</div>
-        <div style="font-size:20px;margin-top:10px">Amount VSYS:</div>
-        <el-input type="number" v-model="amount" auto-complete="off"></el-input>
-      </el-form>
-      <div slot="footer" style="display:flex;justify-content:space-between">
-        <el-button @click="dialogFormVisible = false">Cancel</el-button>
-        <el-button type="primary" @click="donate">Confirm</el-button>
+    <el-dialog :visible.sync="dialogFormVisible" :show-close="false" top="25vh" append-to-body @closed="closeDonate">
+      <div v-if="isShowTips"  slot="title">
+        <div style="width:100%;text-align:right;">
+          <img
+            width="30"
+            src="@/assets/imgs/donate_close.svg"
+            style="cursor:pointer;"
+            @click="dialogFormVisible=false"
+          />
+        </div>
+        <div style="width:100%;text-align:center;font-family: sen-bold;font-size:18px">
+          <div style="width:70%;margin-left:15%;margin-top:15px;">
+           If you would like to support Felix Dictionary you can send DARA (DARA) or VSYS (V Systems). Thanks!
+          </div>
+        </div>
+        <div style="width:100%;display:flex;flex-direction:column;align-items:center;">
+          <div @click="chooseDonationType(true)" style="width:288px;height:43px;background:#FB8809;color:#F3EDE6;font-family: sen-regular;font-size:20px;text-align:center;line-height:43px;border-radius:8px;cursor:pointer;margin-top:35px;">DONATE VSYS</div>
+          <div @click="chooseDonationType(false)" style="width:288px;height:43px;background:#FB8809;color:#F3EDE6;font-family: sen-regular;font-size:20px;text-align:center;line-height:43px;border-radius:8px;cursor:pointer;margin-top:20px;">DONATE DARA</div>
+        </div>
       </div>
+      <div v-else slot="title">
+        <div style="font-family: sen-bold;font-size:18px">Thank you for your donation!</div>
+        <el-form style="margin-top:25px;">
+          <div
+            style="font-family: sen-regular;font-size:18px;"
+          >The money you donate will go into this account:</div>
+          <div style="color:#FB8809;font-family: sen-regular;font-size:18px">{{ isDonateVSYS ?  address : '0xcB613BFC2fA5F14F64024b4E012aBB742AA1B677' }}</div>
+          <div style="font-family: sen-regular;font-size:18px;margin-top:20px;margin-bottom:10px;">Amount {{isDonateVSYS ? 'VSYS' : 'DARA'}}:</div>
+          <el-input type="number" v-model="amount" auto-complete="off"></el-input>
+        </el-form>
+      </div>
+        <div v-if="!isShowTips" slot="footer" style="display:flex;justify-content:space-between">
+          <el-button @click="dialogFormVisible = false" style="border:1px solid #000000;color:#171616;" plain>Cancel</el-button>
+          <el-button @click="donate" style="border:none;background:#FB8809;color:#FFFFFF;" plain>Confirm</el-button>
+        </div>
     </el-dialog>
   </div>
 </template>
 
 <script>
 import BigNumber from "bignumber.js";
-import { reqGetBalance,reqGetLastSaved,reqGetCountNFT,reqGetCountSaved } from "@/api/index";
+import {
+  reqGetBalance,
+  reqGetLastSaved,
+  reqGetCountNFT,
+  reqGetCountSaved
+} from "@/api/index";
 
 export default {
   async created() {
     const balanceData = await reqGetBalance();
     if (balanceData) {
-      let values = Object.values(balanceData)
-      this.address = values[0].address
-      this.balance = BigNumber(values[0].balance).dividedBy(1e8)
+      let values = Object.values(balanceData);
+      this.address = values[0].address;
+      this.balance = BigNumber(values[0].balance).dividedBy(1e8);
     }
     const lastSavedWordsList = await reqGetLastSaved();
     if (lastSavedWordsList) {
-      for (var savedWord in lastSavedWordsList){
-        this.lastSavedWords = this.lastSavedWords + " " + JSON.parse(lastSavedWordsList[savedWord])["word"]+ ", "
+      for (var savedWord in lastSavedWordsList) {
+        this.lastSavedWords =
+          this.lastSavedWords +
+          " " +
+          JSON.parse(lastSavedWordsList[savedWord])["word"] +
+          ", ";
       }
-      this.lastSavedWords = this.lastSavedWords.slice(0, this.lastSavedWords.length-2)
+      this.lastSavedWords = this.lastSavedWords.slice(
+        0,
+        this.lastSavedWords.length - 2
+      );
     }
     this.NFTCount = await reqGetCountNFT();
   },
@@ -70,10 +105,20 @@ export default {
       amount: "",
       dialogFormVisible: false,
       lastSavedWords: "",
-      NFTCount: 0
+      NFTCount: 0,
+      isShowTips: true,
+      isDonateVSYS: true
     };
   },
   methods: {
+    chooseDonationType(val){
+      this.isShowTips = false
+      this.isDonateVSYS = val
+    },
+    closeDonate(){
+      this.isShowTips = true
+      this.amount = ''
+    },
     openUrl(url) {
       window.open(url, "_blank");
     },
@@ -85,36 +130,49 @@ export default {
         return alert("Invalid address");
       } else {
         let res = await window.vsys.request({
-          method: 'send',
-          params:
-              {
-                publicKey: this.$store.state.vsys.wallet.publicKey,
-                recipient: this.address,
-                amount: this.amount,
-                description: "donation"
-              }
-        })
-        if (res.result && res.message === 'OK') {
-          this.dialogFormVisible = false
+          method: "send",
+          params: {
+            publicKey: this.$store.state.vsys.wallet.publicKey,
+            recipient: this.address,
+            amount: this.amount,
+            description: "donation"
+          }
+        });
+        if (res.result && res.message === "OK") {
+          this.dialogFormVisible = false;
           alert("Payment submitted successfully, please refresh page later");
         } else {
-          console.log(res, "cancel")
+          console.log(res, "cancel");
         }
       }
     }
-  },
-  components: {}
+  }
 };
 </script>
 
-<style scoped>
+<style lang="less" scoped>
+.el-button {
+    width:141px;
+    height:37px;
+    line-height:37px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 8px;
+
+    /deep/ span {
+        font-family: sen-regular;
+        font-size:20px;
+        font-weight: normal;
+    }
+}
 @media screen and (min-width: 500px) {
   .placeholderDiv {
     position: relative;
     width: 50px;
   }
   .content {
-    margin-right:150px;
+    margin-right: 150px;
   }
 }
 @media screen and (max-width: 500px) {
@@ -128,9 +186,9 @@ export default {
     margin-right: 20px;
   }
   .content {
-    display:flex;
-    align-items:center;
-    justify-content:space-between;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
   }
 }
 #bottom {
@@ -143,8 +201,9 @@ export default {
   font-size: 1rem;
   z-index: 99;
 }
-.el-dialog__wrapper >>> .el-dialog {
-  min-width: 300px;
+.el-dialog__wrapper /deep/ .el-dialog {
+  width: 489px !important;
+  border-radius: 20px;
 }
 .dontate-btn {
   display: inline-block;
